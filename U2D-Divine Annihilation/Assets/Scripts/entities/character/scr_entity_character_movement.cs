a97d110890df991bf2c9a6b77c7094271f9b325d
Arthur W. Sheldon <trash.block2@gmail.com>
2020-07-11 10:59:13 -0400

    Added basic character movement, Removed redundant files

---
+++ U2D-Divine Annihilation/Assets/Scripts/entities/character/scr_entity_character_movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_entity_character_movement : MonoBehaviour
+{
+    // Setup configurable variables
+    public float movementSpeed = 5f;
+    public Rigidbody2D Rigidbody;
+
+    // Input variables
+    Vector2 movement;
+
+    // Update is called once per frame (Speed is called by FPS)
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    // Update is not tied to FPS but updates at a constant rate
+    void FixedUpdate()
+    {
+        // Movement
+        Rigidbody.MovePosition(Rigidbody.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
