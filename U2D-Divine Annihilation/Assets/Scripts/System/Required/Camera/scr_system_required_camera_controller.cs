2d980264f0db9df6fa7e340fb67fd64e83b4fcd0
Arthur W. Sheldon <trash.block2@gmail.com>
2020-07-11 20:01:07 -0400

    Added camera follow target, Updated player movement

---
+++ U2D-Divine Annihilation/Assets/Scripts/System/Required/Camera/scr_system_required_camera_controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_system_required_camera_controller : MonoBehaviour
+{
+    // Setup camera variables
+    public Transform followTarget;
+    public float zoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, followTarget.transform.position.z-zoom);
+    }
+}
