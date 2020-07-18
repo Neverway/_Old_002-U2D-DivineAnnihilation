459104be62b0b1c56c9f249f870b5a1878b7af54
Arthur W. Sheldon <trash.block2@gmail.com>
2020-07-17 22:20:43 -0400

    Added sprinting

--- U2D-Divine Annihilation/Assets/System/Scripts/entities/character/scr_entity_character_movement.cs
+++ U2D-Divine Annihilation/Assets/System/Scripts/entities/character/scr_entity_character_movement.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class scr_entity_character_movement : MonoBehaviour
 {
     // Setup configurable variables
-    public float movementSpeed = 5f;
+    public float walkSpeed = 5f;
+    public float sprintSpeed = 5f;
+    float movementSpeed;
     public Rigidbody2D Rigidbody;
     public Animator characterAnimator;
 
     // Input variables
     Vector2 movement;
+
+    void Start()
+    {
+        movementSpeed = walkSpeed;
+    }
 
     // Update is called once per frame (Speed is called by FPS)
     void Update()
@@ -18,10 +25,21 @@ public class scr_entity_character_movement : MonoBehaviour
         // Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        
 
         characterAnimator.SetFloat("Horizontal", movement.x);
         characterAnimator.SetFloat("Vertical", movement.y);
         characterAnimator.SetFloat("Speed", movement.sqrMagnitude);
+
+        // sprinting
+        if (Input.GetKeyDown("x"))
+        {
+            movementSpeed = sprintSpeed;
+        }
+        else if (Input.GetKeyUp("x"))
+        {
+            movementSpeed = walkSpeed;
+        }
     }
 
     // Update is not tied to FPS but updates at a constant rate
