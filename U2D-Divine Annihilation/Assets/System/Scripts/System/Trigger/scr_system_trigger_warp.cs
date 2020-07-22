13a123eb81d6b0869bc921562485238d9da9594a
Arthur W. Sheldon <trash.block2@gmail.com>
2020-07-22 15:32:27 -0400

    Added autumn forest indoor tiles, Added warp triggers, Updated Hometown level

---
+++ U2D-Divine Annihilation/Assets/System/Scripts/System/Trigger/scr_system_trigger_warp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_system_trigger_warp : MonoBehaviour
+{
+    public GameObject ExitTarget;
+    public GameObject Player;
+    public bool PlayTransition;
+    
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Player.transform.position = new Vector2 (ExitTarget.transform.position.x, ExitTarget.transform.position.y);
+    }
+}
