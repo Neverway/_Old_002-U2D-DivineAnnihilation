ec645218f4b06b275e46933f89ed02055827bfb7
Arthur W. Sheldon <trash.block2@gmail.com>
2020-07-29 23:33:04 -0400

    Refactored text-box scripts, Added name field to text-box

--- U2D-Divine Annihilation/Assets/Unpacked/Level Objects/Triggers/Interact/Scripts/scr_level_trigger_interact.cs
+++ U2D-Divine Annihilation/Assets/Unpacked/Level Objects/Triggers/Interact/Scripts/scr_level_trigger_interact.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class scr_level_trigger_interact : MonoBehaviour
 {
-
-    public string dialogue;
     private scr_system_hud_textbox_manager DialogueManager;
-
     public string[] dialogueLines;
+    public string[] dialogueLineNames;
+    //public Sprite[] dialogueLinePortraits;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,19 +16,24 @@ public class scr_level_trigger_interact : MonoBehaviour
         DialogueManager = FindObjectOfType<scr_system_hud_textbox_manager>();
     }
 
+
+    // Check if something has been in the trigger
     void OnTriggerStay2D(Collider2D other)
     {
+        // Check if that something is the player
         if(other.gameObject.name == "pre_entity_main_fox_overworld")
         {
-
+            // Check if the player has pressed the action key
             if(Input.GetKeyUp("z"))
             {
-                //DiaMan.ShowBox(dialogue);
+                // Check if the dialogue box is already open
                 if(!DialogueManager.dialogueBoxActive)
                 {
-                    DialogueManager.dialogueLines = dialogueLines;
-                    DialogueManager.currentLine = 0;
-                    DialogueManager.ShowDialogue();
+                    DialogueManager.dialogueLines = dialogueLines; // Pass the dialogue lines value to the manager (don't bother understanding this, it just works so I don't bother messing with it)
+                    DialogueManager.dialogueLineNames = dialogueLineNames;
+                    //DialogueManager.dialogueLinePortraits = dialogueLinePortraits;
+                    DialogueManager.currentLine = 0;               // Reset the current line (in case the dialogue manager failes to)
+                    DialogueManager.ShowDialogue();                // Execute the show dialogue function
                 }
             }
         }
