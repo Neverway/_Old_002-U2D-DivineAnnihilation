22e326bfa40dccc55d5b6b2ff1eface97281de84
Arthur W. Sheldon <trash.block2@gmail.com>
2020-09-23 12:17:40 -0400

    Added sprites to SideCast sheet, Added extra menu control scripts, Started creation of new save & load feature

---
+++ U2D-Divine Annihilation/Assets/Scripts/Menus & Hud/scr_menu_scrollMinusControl.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scr_menu_scrollMinusControl : MonoBehaviour
+{
+    // Referances
+    public Sprite[] frames;
+    public Image spriteRenderer;
+    public bool horizontalScrolling;
+    public bool wrapAround;
+    public bool canGoBack;
+
+
+    // Referances
+    public int currentFrame;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vertical scrolling
+        if (!horizontalScrolling)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (currentFrame == 0 && wrapAround)
+                {
+                    currentFrame = frames.Length; // Up arrow wrap around
+                }
+
+                if (currentFrame != 0)
+                {
+                    currentFrame -= 1; // Up arrow scrolling
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                if (currentFrame == frames.Length - 1 && wrapAround)
+                {
+                    currentFrame = -1; // Up arrow wrap around
+                }
+
+                if (currentFrame != frames.Length - 1)
+                {
+                    currentFrame += 1; // Up arrow scrolling
+                }
+            }
+        }
+
+        // Horizontal scrolling
+        else if (horizontalScrolling)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                currentFrame += 1;
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                currentFrame -= 1;
+            }
+        }
+
+        // Draw current image
+        spriteRenderer.sprite = frames[currentFrame];
+    }
+}
---
+++ U2D-Divine Annihilation/Assets/Scripts/System/scr_system_saveManager.cs
@@ -0,0 +1,100 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_system_saveManager : MonoBehaviour
+{
+    public static scr_system_saveManager instance;
+    public SaveData activeSave;
+    public bool hasLoaded;
+
+
+    private void Awake()
+    {
+        instance = this;
+        Load();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            string dataPath = Application.persistentDataPath;
+            Debug.Log(dataPath);
+            Save();
+        }
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            Load();
+        }
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            DeleteSaveProfile();
+        }
+    }
+
+
+    // Save the game data to the current save profile
+    public void Save()
+    {
+        string dataPath = Application.persistentDataPath;
+        var serializer = new XmlSerializer(typeof(SaveData));
+        var stream = new FileStream(dataPath + "/" + activeSave.saveProfileName + ".dasp", FileMode.Create);
+        serializer.Serialize(stream, activeSave);
+        stream.Close();
+
+        Debug.Log("Saved information to .DASP");
+    }
+
+    // Load the game data to the current save profile
+    public void Load()
+    {
+        string dataPath = Application.persistentDataPath;
+        if (System.IO.File.Exists(dataPath + "/" + activeSave.saveProfileName + ".dasp"))
+        {
+            var serializer = new XmlSerializer(typeof(SaveData));
+            var stream = new FileStream(dataPath + "/" + activeSave.saveProfileName + ".dasp", FileMode.Open);
+            activeSave = serializer.Deserialize(stream) as SaveData;
+            stream.Close();
+
+            Debug.Log("Loaded information from .DASP");
+            hasLoaded = true;
+        }
+    }
+
+    public void DeleteSaveProfile()
+    {
+        string dataPath = Application.persistentDataPath;
+        if (System.IO.File.Exists(dataPath + "/" + activeSave.saveProfileName + ".dasp"))
+        {
+            File.Delete(dataPath + "/" + activeSave.saveProfileName + ".dasp");
+
+            Debug.Log("Deleted current .DASP");
+        }
+    }
+}
+
+[System.Serializable]
+public class SaveData
+{
+    // Player Data
+    public string saveProfileName;
+    public Vector2 playerSavePosition;
+    public float playerHealth;
+    public bool partyMemberOneFollowing;
+
+    // Chapter Data
+
+}
