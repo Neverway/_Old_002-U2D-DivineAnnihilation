dab4dd115fd1ff28edf20f386c22c7235e4e488d
Arthur W. Sheldon <trash.block2@gmail.com>
2020-09-23 12:17:40 -0400

    Added sprites to SideCast sheet, Added extra menu control scripts, Started creation of new save & load feature

---
+++ U2D-Divine Annihilation/Assets/Scripts/Menus & Hud/scr_menu_saveLoad.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_menu_saveLoad : MonoBehaviour
+{
+    public GameObject target;
+    private scr_menu_scrollMinusControl menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu = target.GetComponent<scr_menu_scrollMinusControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("z"))
+        {
+            if (menu.currentFrame == 0)
+            {
+                Debug.Log("Save Profile 1");
+            }
+            if (menu.currentFrame == 1)
+            {
+                Debug.Log("Save Profile 2");
+            }
+            if (menu.currentFrame == 2)
+            {
+                Debug.Log("Save Profile 3");
+            }
+            if (menu.currentFrame == 3)
+            {
+                Debug.Log("Save Profile 4");
+            }
+        }
+    }
+}
---
+++ U2D-Divine Annihilation/Assets/Scripts/Triggers/scr_trigger_save.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_trigger_save : MonoBehaviour
+{
+    private scr_system_saveManager saveManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        saveManager = FindObjectOfType<scr_system_saveManager>();   // Find the dialogue manager script
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Check if something has been in the trigger
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKeyDown("z"))
+        {
+            saveManager.Save();
+        }
+    }
+}
