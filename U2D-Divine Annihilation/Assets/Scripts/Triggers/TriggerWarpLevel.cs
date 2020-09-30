4fcae826d67cef4659a0941ad578063f7bb6a462
Arthur W. Sheldon <trash.block2@gmail.com>
2020-09-30 11:06:33 -0400

    Fixed title scene

--- U2D-Divine Annihilation/Assets/Scripts/Triggers/TriggerWarpLevel.cs
+++ U2D-Divine Annihilation/Assets/Scripts/Triggers/TriggerWarpLevel.cs
@@ -2,19 +2,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerWarpLevel : MonoBehaviour
 {
-    public GameObject ExitTarget;
+    public float nextRoomX;
+    public float nextRoomY;
+    public string loadRoom;
     public GameObject Player;
     public bool PlayTransition;
-    
+
+    public void Update()
+    {
+        PlayerPrefs.SetFloat("NextRoomX", nextRoomX);
+        PlayerPrefs.SetFloat("NextRoomY", nextRoomY);
+    }
+
     public void OnTriggerEnter2D(Collider2D other)
     {
         // Check if that something is the player
-        //if (other.gameObject.name == "pre_entity_main_fox_overworld")
-        //{
-            other.transform.position = new Vector2(ExitTarget.transform.position.x, ExitTarget.transform.position.y);
-        //}
+        if (other.gameObject.name == "Entity Fox")
+        {
+            SceneManager.LoadScene(loadRoom);
+        }
     }
 }
