9da83c49d4d4b2dd017301ac05e60938649b6adf
Arthur W. Sheldon <trash.block2@gmail.com>
2020-09-30 09:04:16 -0400

    Updated C1S1, Added updated C1S2, Updated scene template

---
+++ U2D-Divine Annihilation/Assets/Scripts/Triggers/TriggerSave.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerSave : MonoBehaviour
+{
+    private SaveManager saveManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        saveManager = FindObjectOfType<SaveManager>();   // Find the dialogue manager script
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Check if something has been in the trigger
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKeyDown("z"))
+        {
+            saveManager.activeSave.scene = SceneManager.GetActiveScene().name;
+            saveManager.Save();
+        }
+    }
+}
