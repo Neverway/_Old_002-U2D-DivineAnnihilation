331358fb19249e1d9c6742d7c60bc671f3aa33dd
Arthur W. Sheldon <trash.block2@gmail.com>
2020-10-26 11:23:56 -0400

    Cleaned up project assets, Added music files to project assets list

---
+++ U2D-Divine Annihilation/Assets/4.Scripts/Battle/BattleBackgroundLooper.cs
@@ -0,0 +1,24 @@
+﻿// Included Libraries
+using UnityEngine;
+
+/* Battle background looper
+ * ---------------------
+ * This script is applied to the background looper trigger in a battle scene
+ * It seemlessly loops the background object along the X-axis
+*/
+public class BattleBackgroundLooper : MonoBehaviour
+{
+    // Public variables
+    public float offset = 8; // How far back will the background object be pushed
+
+
+    // Check if something has collided with the trigger
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if that something is the background object
+        if (other.gameObject.name == "Background")
+        {
+                other.transform.position = new Vector2(other.transform.position.x+offset, other.transform.position.y); // Set the background object to the new X position
+        }
+    }
+}
