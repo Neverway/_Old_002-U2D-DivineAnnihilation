47559a74869d388e7162e0f4eca3ff833c4d21e3
Arthur W. Sheldon <trash.block2@gmail.com>
2020-10-27 23:02:29 -0400

    Added battle turn selection arrows, Added wake up sequence

--- U2D-Divine Annihilation/Assets/4.Scripts/Battle/BattleActionFunctions.cs
+++ U2D-Divine Annihilation/Assets/4.Scripts/Battle/BattleActionFunctions.cs
@@ -1,4 +1,6 @@
 ﻿// Included Libraries
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,34 +13,78 @@ using UnityEngine.SceneManagement;
 public class BattleActionFunctions : MonoBehaviour
 {
     // Class references
+    public GameObject actionMenu;
+    public GameObject enemySelectionMenu;
     public GameObject configTarget;                 // Get a reference to the Config object in the scene
     private SaveManager saveManager;                // Get a reference to the SaveManager script
+    public GameObject battleControllerTarget;                 // Get a reference to the Config object in the scene
+    private BattleTurnManager turnManager;                // Get a reference to the SaveManager script
     private MenuScrollString menuScrollString;      // Get a reference to the MenuScrollString script
+    public bool acceptingInput = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
         saveManager = configTarget.GetComponent<SaveManager>(); // Set a reference to the SaveManager script on the Config object in the scene
+        turnManager = battleControllerTarget.GetComponent<BattleTurnManager>(); // Set a reference to the SaveManager script on the Config object in the scene
         menuScrollString = GetComponent<MenuScrollString>();    // Set a reference to the MenuScrollString script
+    }
+
+
+    // Key press delay
+    IEnumerator acceptInput()
+    {
+        yield return new WaitForSeconds(1);     // The delay until it is accepting input again
+        acceptingInput = true;                  // Allow input again
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        // Check to see if the Flee menu option is selected
-        if (menuScrollString.currentSelection == 3 && Input.GetKeyDown("z"))
+        // Check to see if the Defend menu option is selected
+        if (menuScrollString.currentSelection == 0 && Input.GetKeyDown("z") && acceptingInput == true)
         {
-            Flee(); // Execute the Flee class from this script
+            acceptingInput = false;                  // Allow input again
+            StartCoroutine("acceptInput");                                  // Activate the keypress delay
+            Attack(); // Execute the Defend class from this script
         }
+        // Check to see if the Defend menu option is selected
+        if (menuScrollString.currentSelection == 1 && Input.GetKeyDown("z") && acceptingInput == true)
+        {
+            acceptingInput = false;                  // Allow input again
+            Defend(); // Execute the Defend class from this script
+            StartCoroutine("acceptInput");                                  // Activate the keypress delay
+        }
+        // Check to see if the Flee menu option is selected
+        if (menuScrollString.currentSelection == 3 && Input.GetKeyDown("z") && acceptingInput == true)
+        {
+            acceptingInput = false;                  // Allow input again
+            Flee(); // Execute the Flee class from this script
+            StartCoroutine("acceptInput");                                  // Activate the keypress delay
+        }
+    }
+
+    // Test if the party is able to flee, and if so set them to their pre-battle locations
+    public void Attack()
+    {
+        enemySelectionMenu.SetActive(true);
+        actionMenu.SetActive(false);
+    }
+
+
+    // Test if the party is able to flee, and if so set them to their pre-battle locations
+    public void Defend()
+    {
+        turnManager.NextTurn();
     }
 
 
     // Test if the party is able to flee, and if so set them to their pre-battle locations
     public void Flee()
     {
-        if (Random.Range(0, 100) <= PlayerPrefs.GetFloat("fleePercent"))
+        if (UnityEngine.Random.Range(0, 100) <= PlayerPrefs.GetFloat("fleePercent"))
         {
             SceneManager.LoadScene(PlayerPrefs.GetString("TempScene")); // Load the pre-battle scene
         }
---
+++ U2D-Divine Annihilation/Assets/4.Scripts/Character/CharacterWakeUp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterWakeUp : MonoBehaviour
+{
+    public bool animationFinished;
+    public GameObject playerCharacter;
+    public GameObject playerCharacterShadow;
+    public GameObject configTarget;
+    private SaveManager saveManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        saveManager = configTarget.GetComponent<SaveManager>();
+        if (saveManager.activeSave.hasWokenUp)
+        {
+            configTarget.GetComponent<SystemConfigManager>().overrideCanMove = false;
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            configTarget.GetComponent<SystemConfigManager>().overrideCanMove = true;
+            playerCharacter.GetComponent<SpriteRenderer>().enabled = false;
+            playerCharacterShadow.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animationFinished)
+        {
+            configTarget.GetComponent<SystemConfigManager>().overrideCanMove = false;
+            playerCharacter.GetComponent<SpriteRenderer>().enabled = true;
+            playerCharacterShadow.GetComponent<SpriteRenderer>().enabled = true;
+            gameObject.SetActive(false);
+            saveManager.activeSave.hasWokenUp = true;
+        }
+    }
+}
