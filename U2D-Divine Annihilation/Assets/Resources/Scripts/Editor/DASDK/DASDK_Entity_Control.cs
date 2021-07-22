ef38cb51333d04efd3e8ad5df87dc95d9e5557e7
Arthur W. Sheldon <trash.block2@gmail.com>
2021-07-22 04:10:48 -0400

    Added DA_Entity_Control, Added player movement and animation handler to DA_Entity_Control, Updated over-world template

---
+++ U2D-Divine Annihilation/Assets/Resources/Scripts/Editor/DASDK/DASDK_Entity_Control.cs
@@ -0,0 +1,144 @@
+﻿//=========== Written by Arthur W. Sheldon AKA Lizband_UCC ====================
+//
+// Purpose: Give funtionality to an entity
+// Applied to: A entity object in an overworld scene
+// Parent script: DA_Entity_Control
+//
+//=============================================================================
+
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DA_Entity_Control))]
+public class DASDK_Entity_Control : Editor
+{
+    // Setup dropdown array
+    string[] _choices = { "Character", "Enemy", "Player" };
+    int _choiceIndex;
+
+    // Base variables
+    SerializedProperty entityName;
+    SerializedProperty entityAnimator;
+    SerializedProperty idleUp;
+    SerializedProperty idleDown;
+    SerializedProperty idleLeft;
+    SerializedProperty idleRight;
+    SerializedProperty choiceValue;
+    DA_Entity_Control entityControl;
+
+    SerializedProperty shelfSprite;
+    SerializedProperty inventoryType;
+
+
+    // Find parent script variables
+    void OnEnable()
+    {
+        // Find Base variables
+        entityName = serializedObject.FindProperty("entityName");
+        entityAnimator = serializedObject.FindProperty("entityAnimator");
+        idleUp = serializedObject.FindProperty("idleUp");
+        idleDown = serializedObject.FindProperty("idleDown");
+        idleLeft = serializedObject.FindProperty("idleLeft");
+        idleRight = serializedObject.FindProperty("idleRight");
+        choiceValue = serializedObject.FindProperty("choiceValue");
+
+        shelfSprite = serializedObject.FindProperty("shelfSprite");
+        inventoryType = serializedObject.FindProperty("inventoryType");
+    }
+
+
+    // Draw the custom editor
+    public override void OnInspectorGUI()
+    {
+        // Drop down and script reference
+        entityControl = (DA_Entity_Control)target;                                      // Set the current script to the target class
+        _choiceIndex = entityControl.choiceValue;                                       // Save the dropdown state
+        _choiceIndex = EditorGUILayout.Popup("Entity Type", _choiceIndex, _choices);    // Draw the dropdown button
+        EditorGUILayout.Space();                                                                      // Add a divider
+
+        serializedObject.Update();
+
+        //Display dropdown choices
+        ChoiceOne();
+        ChoiceTwo();
+        ChoiceThree();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+
+    // Dropdown choices
+    public void ChoiceOne()
+    {
+        if (_choiceIndex == 0)
+        {
+            EditorGUILayout.HelpBox("Passive NPC's & party members/followers", MessageType.None);   // Description
+            BaseVaraibles();
+
+            entityControl.isFollower = EditorGUILayout.Toggle("Is a follower", entityControl.isFollower);   // Sprites over animator
+
+            // Follower
+            if (entityControl.isFollower)
+            {
+                EditorGUILayout.PropertyField(shelfSprite);
+            }
+
+            entityControl.choiceValue = 0;
+        }
+    }
+
+
+    public void ChoiceTwo()
+    {
+        if (_choiceIndex == 1)
+        {
+            EditorGUILayout.HelpBox("Hostile NPC's that hunt down or attack the player", MessageType.None);     // Description
+            BaseVaraibles();
+
+            entityControl.choiceValue = 1;
+        }
+    }
+
+
+    public void ChoiceThree()
+    {
+        if (_choiceIndex == 2)
+        {
+            EditorGUILayout.HelpBox("Playable characters that are controled by the player", MessageType.None);  // Description
+            BaseVaraibles();
+
+            EditorGUILayout.PropertyField(shelfSprite);
+            EditorGUILayout.PropertyField(inventoryType);
+
+            entityControl.choiceValue = 2;
+        }
+    }
+
+
+    public void BaseVaraibles()
+    {
+        EditorGUILayout.PropertyField(entityName);  // Entity name
+        EditorGUILayout.FloatField("Max Health", entityControl.maxHealth); // Entity Health
+        entityControl.useSpritesOverAnimator = EditorGUILayout.Toggle("Sprites Over Animator", entityControl.useSpritesOverAnimator);   // Sprites over animator
+
+        // Animator
+        if (!entityControl.useSpritesOverAnimator)
+        {
+            EditorGUILayout.PropertyField(entityAnimator);
+        }
+
+        // Sprites over animator
+        else if (entityControl.useSpritesOverAnimator)
+        {
+            EditorGUILayout.PropertyField(idleUp);
+            EditorGUILayout.PropertyField(idleDown);
+            EditorGUILayout.PropertyField(idleLeft);
+            EditorGUILayout.PropertyField(idleRight);
+        }
+
+        EditorGUILayout.FloatField("Walk Speed", entityControl.walkSpeed); // Walk Speed
+        EditorGUILayout.FloatField("Sprint Speed", entityControl.sprintSpeed); // Sprint Speed
+
+        EditorGUILayout.Space();                                                       // Add a divider
+    }
+}
---
+++ U2D-Divine Annihilation/Assets/Resources/Scripts/Editor/DASDK/DASDK_Tool_AssetBrowser.cs
@@ -0,0 +1,29 @@
+﻿//=========== Written by Arthur W. Sheldon AKA Lizband_UCC ====================
+//
+// Purpose: 
+//
+//=============================================================================
+
+using UnityEngine;
+using UnityEditor;
+
+public class DASDK_Tool_AssetBrowser : EditorWindow
+{
+    // Public variables
+
+    // Private variables
+
+    // Reference variables
+
+
+    [MenuItem("DASDK/Asset Browser")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(DASDK_Tool_AssetBrowser));
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("AssetBrowser", EditorStyles.boldLabel);
+    }
+}
