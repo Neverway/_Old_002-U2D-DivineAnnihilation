312ae33da81325995b4981e596f5c0a6770ff3e4
Liz Miyuna <trash.block2@gmail.com>
2021-09-18 15:52:17 -0400

    Updated C1S1, Fixed interact event triggers

--- U2D-Divine Annihilation/Assets/Resources/Scripts/DA_Trigger_Interact.cs
+++ U2D-Divine Annihilation/Assets/Resources/Scripts/DA_Trigger_Interact.cs
@@ -57,9 +57,8 @@ public class DA_Trigger_Interact : MonoBehaviour
 
     void Update()
     {
-        if (inTrigger && Input.GetKeyDown(inputManager.controls["Interact"]) && acceptingInput == true && !initialized && !menuManager.menuActive)
+        if (inTrigger && Input.GetKeyDown(inputManager.controls["Interact"]) && acceptingInput == true && !initialized && !menuManager.menuActive && !eventTrigger)
         {
-            print("An interact trigger has been activated. If you are reading this during the test, then you have found a bug.");
             textboxManager.targetTrigger = gameObject;
             acceptingInput = false;     // Enable the keypress delay
                                         // Check if the dialogue box is already open
@@ -77,11 +76,32 @@ public class DA_Trigger_Interact : MonoBehaviour
                 print("Bink");
             }
         }
+<<<<<<< HEAD
         if (testing[0] != lineText[0])
         {
             Debug.LogError(gameObject.name + ": SHIT'S BROKE! Something just overwrote an interaction triggers events!");
             Debug.LogWarning("Logged: " + testing[0]);
             Debug.LogWarning("Line: " + lineText[0]);
+=======
+        if (inTrigger && acceptingInput == true && !initialized && !menuManager.menuActive && eventTrigger && !eventActive)
+        {
+            textboxManager.targetTrigger = gameObject;
+            acceptingInput = false;     // Enable the keypress delay
+            eventActive = true;
+                                        // Check if the dialogue box is already open
+            if (!textboxManager.textboxActive)
+            {
+                textboxManager.lineText = lineText;                  // Pass the dialogue lines value to the manager (don't bother understanding this, it just works so I don't bother messing with it)
+                textboxManager.lineName = lineName;          // Pass the dialogue line names value to the manager
+                textboxManager.linePortrait = linePortrait;  // Pass the dialogue line portraits value to the manager
+                textboxManager.currentTextLine = 0;                            // Reset the current line (in case the dialogue manager failes to)
+                textboxManager.ShowDialogue();                                 // Execute the show dialogue function
+                StartCoroutine(acceptInput());                                 // Activate the keypress delay
+                textboxManager.textboxActive = true;
+                //textboxManager.targetTrigger = gameObject;
+                //textboxManager.destroyOnFinish = destroyOnFinish;
+            }
+>>>>>>> 2d8ae88a1 (Updated C1S1, Fixed interact event triggers)
         }
     }
 
@@ -102,6 +122,7 @@ public class DA_Trigger_Interact : MonoBehaviour
             textboxManager.targetTrigger = null;
             textboxManager.currentlyOverlappedTrigger = null;
             inTrigger = false;
+            eventActive = false;
         }
     }
     public void EnableTrigger()
