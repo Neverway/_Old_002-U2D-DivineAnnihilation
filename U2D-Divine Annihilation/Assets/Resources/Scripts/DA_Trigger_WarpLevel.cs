7814dd70706097534433c43b14ece7702bf6c46e
Liz Miyuna <trash.block2@gmail.com>
2021-10-28 16:02:12 -0400

    Updated Template_Overworld, Added level warp triggers

--- U2D-Divine Annihilation/Assets/Resources/Scripts/DA_Trigger_WarpLevel.cs
+++ U2D-Divine Annihilation/Assets/Resources/Scripts/DA_Trigger_WarpLevel.cs
@@ -8,19 +8,44 @@
 //
 //=============================================================================
 
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DA_Trigger_WarpLevel : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float nextRoomX;
+    public float nextRoomY;
+    public string loadRoom;
+    private OTU_System_SaveManager saveManager;
+    private OTU_System_TransitionManager transitionManager;
+
+
     void Start()
     {
-        
+        saveManager = FindObjectOfType<OTU_System_SaveManager>();   // Find the dialogue manager script
+        transitionManager = FindObjectOfType<OTU_System_TransitionManager>();   // Find the dialogue manager script
     }
 
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator ChangeScene()
     {
-        
+        yield return new WaitForSeconds(transitionManager.fadeStayDelay);     // The delay until it is accepting input again
+        PlayerPrefs.SetInt("LoadingNewRoom", 1);
+        saveManager.activeSave2.scene = loadRoom;
+        SceneManager.LoadScene(loadRoom);
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            transitionManager.FadeIn("");
+            PlayerPrefs.SetFloat("NextRoomX", nextRoomX);
+            PlayerPrefs.SetFloat("NextRoomY", nextRoomY);
+            StartCoroutine("ChangeScene");
+        }
     }
 }
