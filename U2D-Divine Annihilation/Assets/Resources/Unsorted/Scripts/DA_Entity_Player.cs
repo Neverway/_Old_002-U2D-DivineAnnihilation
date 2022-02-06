c561262aef4e0561d0771fcc21048e43f8bda55a
Liz Miyuna <neverwayhelp@gmail.com>
2022-02-06 05:02:43 -0500

    sorted project resources

---
+++ U2D-Divine Annihilation/Assets/Resources/Unsorted/Scripts/DA_Entity_Player.cs
@@ -0,0 +1,21 @@
+//=========== Written by Arthur W. Sheldon AKA Lizband_UCC ====================
+//
+// SID: 
+// Purpose: 
+// Applied to: 
+// Editor script: 
+// Notes: 
+//
+//=============================================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Cinemachine;
+using Pathfinding;
+
+public class DA_Entity_Player : MonoBehaviour
+{
+}
