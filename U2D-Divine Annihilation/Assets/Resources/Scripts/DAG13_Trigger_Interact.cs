1936366e977cccb358d6e46dbda8b6a95d037381
Lizband <Trash.block2@gmail.com>
2022-09-05 17:06:49 -0400

    Added eventTrigger parameter to interact trigger script

--- U2D-Divine Annihilation/Assets/Resources/Scripts/DAG13_Trigger_Interact.cs
+++ U2D-Divine Annihilation/Assets/Resources/Scripts/DAG13_Trigger_Interact.cs
@@ -1,40 +1,43 @@
 //=========== Written by Arthur W. Sheldon AKA Lizband_UCC ====================
 //
-// Purpose: 
-// Applied to: 
+// Purpose: Show a textbox when interacting and inside of the trigger
+// Applied to: A interaction trigger
 // Editor script: 
 // Notes: 
-// Text Markups
-// Color:     {c(#ffffff)}
-// Bold:      {b}
-// Italic:    {i}
-// Underline: {u}
-// Regular:   {r} (returns the text to it's default color and removes any styles)
-    
-// Textbox content Markups
-// Speed:     {s(1)} (1 is the default speed, 2 is the maximum speed)
-// Portrait:  {p} (goes to the next portrait in the portrait array)
+//		Text Markups
+//		Color:     <color=#ffffff)> or <color="blue">
+//		Bold:      <b>
+//		Italic:    <i>
+//		Underline: <u>
+//		Regular:   {r} (returns the text to it's default color and removes any styles)
+//
+//		Textbox content Markups
+//		Speed:     {s=1} (1 is the default speed, 2 is the maximum speed)
+//		Portrait:  {p} (goes to the next portrait in the portrait array)
 //
 //=============================================================================
 
-using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Events;
 
+[RequireComponent(typeof(Collider2D))]
 public class DAG13_Trigger_Interact : MonoBehaviour
 {
     //=-----------------=
     // Public variables
     //=-----------------=
-    [SerializeField]public DAG13_TextboxData textboxData;
+    [SerializeField] private DAG13_TextboxData textboxData;
+    
+    [Tooltip("If true, the trigger will activate when entered")]
+    [SerializeField] private bool eventTrigger;
 
 
     //=-----------------=
     // Private variables
     //=-----------------=
-    public bool inTrigger;
-    public bool active;
+    private bool inTrigger; // Is true if the player is in the trigger
+    private bool activated; // Disables the trigger after activation, can be re-enabled by firing the Reactivate function
+    private bool eventTriggered; // Keeps the event trigger from firing again until the player exits the trigger first
 
 
     //=-----------------=
@@ -49,20 +52,35 @@ public class DAG13_Trigger_Interact : MonoBehaviour
     //=-----------------=
     private void Start()
     {
+	    // Find references
 	    input = FindObjectOfType<NUPInput>();
 	    textboxManager = FindObjectOfType<DAG13_System_TextboxManager>();
     }
 
     private IEnumerator ActivationDelay()
     {
+	    // Re-enable the trigger after a small amount of time has passed
+	    // This is so the trigger won't immediately fire again if it's a repeatable interaction
 	    yield return new WaitForSeconds(0.5f);
-	    active = false;
+	    activated = false;
     }
 
     private void Update()
     {
-	    if (!inTrigger || !input.GetKeyDown("Interact") || textboxManager.active || active) return;
-	    TransmitDataToTextboxManager();
+	    if (!inTrigger || textboxManager.active || activated) return;
+	    
+	    // Triggered via activation
+	    if (input.GetKeyDown("Interact") && !eventTrigger)
+	    {
+		    TransmitDataToTextboxManager();
+	    }
+	    
+	    // Triggered via event
+	    else if (eventTrigger && !eventTriggered)
+	    {
+		    eventTriggered = true;
+		    TransmitDataToTextboxManager();
+	    }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -75,6 +93,8 @@ public class DAG13_Trigger_Interact : MonoBehaviour
     {
 	    if (!other.CompareTag("Player")) return;
 	    inTrigger = false;
+	    if (eventTrigger)
+		    eventTriggered = false;
     }
 
 
@@ -83,7 +103,7 @@ public class DAG13_Trigger_Interact : MonoBehaviour
     //=-----------------=
     private void TransmitDataToTextboxManager()
     {
-	    active = true;
+	    activated = true;
 	    textboxManager.textboxData = textboxData;
 	    textboxManager.StartTextbox();
     }
